4f2ada47601169ecff1f0e67029b76f215036d2e
ErykBabayan <erykbabayan@gmail.com>
2023-05-20 16:36:34 +0200

    add logic for daily pings

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,49 @@
-console.log("hello")
+
+import {TextChannel} from "discord.js";
+import config from "./config";
+import { Client, GatewayIntentBits } from "discord.js";
+
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildPresences,
+    ],
+});
+const channelId: string = "1097919505407152168";
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user?.tag}`);
+    scheduleMessage();
+});
+
+client.login(config.DISCORD_TOKEN);
+
+function scheduleMessage() {
+    const now = new Date();
+    const targetTime:any = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 20, 0, 0);
+    const timeUntilTarget = targetTime.getTime() - now.getTime();
+    if (timeUntilTarget < 0) {
+        // If the target time has already passed today set target date for tomorrow
+        targetTime.setDate(targetTime.getDate() + 1);
+    }
+   // const timeUntilNextDay = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
+
+    setTimeout(() => {
+        sendMessage();
+        scheduleMessage();
+    }, targetTime.getTime() - now.getTime());
+
+    // setTimeout(() => {
+    //     sendMessage();
+    //     scheduleMessage();
+    // }, timeUntilNextDay);
+}
+
+function sendMessage() {
+    const channel = client.channels.cache.get(channelId);
+    (channel as TextChannel).send('@here Daily : Co dzisiaj zrobiliście?');
+}
